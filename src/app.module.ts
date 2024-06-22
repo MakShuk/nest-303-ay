e7e7114d6fecc6897f76cbf8524b79543becc2b0
Maksim Shuklin <makshuk@my.com>
2024-06-22 11:04:57 +0300

    chore: Add LoggerService for logging errors, warnings, and info

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './service/logger/logger.service';
+
+const loggerServiceProvider = {
+  provide: LoggerService,
+  useValue: new LoggerService('app'),
+};
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, loggerServiceProvider],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './service/logger/logger.service';
+
 
 @Injectable()
 export class AppService {
+  constructor(private readonly log: LoggerService) { }
+  
   getHello(): string {
+    this.log.info('Hello World!');
     return 'Hello World!';
   }
 }
